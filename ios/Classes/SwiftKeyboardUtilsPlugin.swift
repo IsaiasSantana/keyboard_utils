4820776785211b53fd4dd61d6790e1449e72721f
Isaias Santana <isds.santana@gmail.com>
2019-10-27 01:01:22 -0300

    update return value

--- ios/Classes/SwiftKeyboardUtilsPlugin.swift
+++ ios/Classes/SwiftKeyboardUtilsPlugin.swift
@@ -1,31 +1,20 @@
 import Flutter
 import UIKit
 
-struct KeyboardOptions: Encodable {
-    let isKeyboardOpen: Bool
-    let keyboardHeight: Double
-    
-    enum CodingKeys: String, CodingKey {
-        
-        case isKeyboardOpen
-        case keyboardHeight
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(isKeyboardOpen, forKey: .isKeyboardOpen)
-        try container.encode(keyboardHeight, forKey: .keyboardHeight)
-    }
-}
+//
+//  SwiftKeyboardUtilsPlugin.swift
+//  keyboard_utils
+//
+//  Created by Isaías Santana on 25/10/19.
+//
 
 public class SwiftKeyboardUtilsPlugin: NSObject, FlutterPlugin ,FlutterStreamHandler {
     private var eventSink: FlutterEventSink?
     private var isKeyboardOpen = false
-    private var isListening = false
     
     public override init() {
         super.init()
-        
+
         registerEvents()
     }
 
@@ -37,7 +26,6 @@ public class SwiftKeyboardUtilsPlugin: NSObject, FlutterPlugin ,FlutterStreamHan
     
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         eventSink = events;
-        isListening = true
         
         if isKeyboardOpen {
             events(true)
@@ -48,7 +36,6 @@ public class SwiftKeyboardUtilsPlugin: NSObject, FlutterPlugin ,FlutterStreamHan
 
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
         eventSink = nil
-        isListening = false
         return nil
     }
 
@@ -57,45 +44,64 @@ public class SwiftKeyboardUtilsPlugin: NSObject, FlutterPlugin ,FlutterStreamHan
                                                selector: #selector(keyboardWillShow),
                                                name: NSNotification.Name.UIKeyboardWillShow,
                                                object: nil)
-        
+
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(keyboardWillHide),
                                                name: NSNotification.Name.UIKeyboardWillHide,
                                                object: nil)
-        
+
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(keyBoardDidShow),
                                                name: NSNotification.Name.UIKeyboardDidShow,
                                                object: nil)
     }
-    
+
     private func unregisterEvenst() {
         guard eventSink != nil else {
             return
         }
-        
+
         NotificationCenter.default.removeObserver(self)
     }
+    
+    private func convertKeyboardOptionsToString(_ keyboardOptions: KeyboardOptions) -> String? {
+       if let encodedData = try? JSONEncoder().encode(keyboardOptions),
+            let jsonString = String(data: encodedData, encoding: .utf8) {
+            return jsonString
+        }
+        
+        return nil
+    }
 
-    @objc private func checkIsKeyboardOpen() {
+    @objc private func checkIsKeyboardOpen(notification: Notification) {
         if !isKeyboardOpen {
             isKeyboardOpen = true
-            eventSink?(true)
+            if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+                let keyboardRectangle = keyboardFrame.cgRectValue
+                let keyboardHeight = keyboardRectangle.height
+                let keyboardOptions = KeyboardOptions(isKeyboardOpen: true, keyboardHeight: Double(keyboardHeight))
+                if let jsonString = convertKeyboardOptionsToString(keyboardOptions) {
+                   eventSink?(jsonString)
+                }
+            }
         }
     }
 
-    @objc private func keyBoardDidShow() {
-        checkIsKeyboardOpen()
+    @objc private func keyBoardDidShow(notification: Notification) {
+        checkIsKeyboardOpen(notification: notification)
     }
 
-    @objc private func keyboardWillShow() {
-       checkIsKeyboardOpen()
+    @objc private func keyboardWillShow(notification: Notification) {
+       checkIsKeyboardOpen(notification: notification)
     }
-    
+
     @objc private func keyboardWillHide() {
         if isKeyboardOpen {
             isKeyboardOpen = false
-            eventSink?(false)
+            let keyboardOptions = KeyboardOptions(isKeyboardOpen: false, keyboardHeight: 0.0)
+            if let jsonString = convertKeyboardOptionsToString(keyboardOptions) {
+               eventSink?(jsonString)
+            }
         }
     }
 }
