6263d3f9225473d50e6d8bdf44173d9ef55955aa
Isaias Santana <isds.santana@gmail.com>
2020-03-02 22:35:40 -0300

    Create create a inherited widget.

---
+++ lib/keyboard_aware/keyboard_config_ inherited_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_utils/keyboard_options.dart';
+
+class KeyboardConfigInheritedWidget extends InheritedWidget {
+  const KeyboardConfigInheritedWidget({this.keyboardConfig, Widget child})
+      : super(child: child);
+
+  final KeyboardOptions keyboardConfig;
+
+  @override
+  bool updateShouldNotify(KeyboardConfigInheritedWidget oldWidget) {
+    return keyboardConfig != oldWidget.keyboardConfig;
+  }
+
+  static KeyboardConfigInheritedWidget of(BuildContext context) => context
+      .dependOnInheritedWidgetOfExactType<KeyboardConfigInheritedWidget>();
+}
